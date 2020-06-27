2e90e89704f5cc93d60bff18f69c3722aec99936
Nikki <archiektor@gmail.com>
2020-06-27 08:53:21 +0200

    Updates

---
+++ precache-manifest.8644f52ec0324d27a34ec8648713f47a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e28e2e3c37b6994d1d908941485ea5",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "225836a3aa377d0671a4",
+    "url": "/Counter/static/css/main.a79be5d0.chunk.css"
+  },
+  {
+    "revision": "44e1d6dcc048827e551a",
+    "url": "/Counter/static/js/2.d47bce48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter/static/js/2.d47bce48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225836a3aa377d0671a4",
+    "url": "/Counter/static/js/main.884a6298.chunk.js"
+  },
+  {
+    "revision": "4a2bea4ebda10b11d1d6",
+    "url": "/Counter/static/js/runtime-main.a4bce0b8.js"
+  }
+]);
